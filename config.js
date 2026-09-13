464e2481df69d79a00ca47d35ea9417c5c648343
Wildtattooo <kellycarolina4615@gmail.com>
2026-09-13 22:29:21 +0100

    Refactor WILD_CONFIG for better readability

--- config.js
+++ config.js
@@ -1,1 +1,6 @@
-window.WILD_CONFIG={whatsappNumber:"351912886085",siteUrl:"https://wildtattoo.pt",supabaseUrl:"",supabaseAnonKey:""};
+window.WILD_CONFIG = {
+  whatsappNumber: "351912886085",
+  siteUrl: "https://wildtattoo.pt",
+  supabaseUrl: "https://ddmzdqoxesbdetkcgctx.supabase.co",
+  supabaseAnonKey: "sb_publishable_IvkoWIwDU55bM5lZl94jdg_b3uXGamY"
+};
