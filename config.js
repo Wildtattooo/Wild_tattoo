4229fae03dfd5b4da62d2b4d3f673ed90edccc0b
Wildtattooo <kellycarolina4615@gmail.com>
2026-09-13 22:31:08 +0100

    Update supabaseUrl in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.WILD_CONFIG = {
   whatsappNumber: "351912886085",
   siteUrl: "https://wildtattoo.pt",
-  supabaseUrl: "https://ddmzdqoxesbdetkcgctx.supabase.co",
+  supabaseUrl: "https://ddmzdqoxesbdetkcgotx.supabase.co",
   supabaseAnonKey: "sb_publishable_IvkoWIwDU55bM5lZl94jdg_b3uXGamY"
 };
